d8e66022ceb3b12a9015dc56b3c6501d5ce5afc7
MaTTeK6 <62448676+MaTTeK6@users.noreply.github.com>
2020-04-21 21:41:59 +0200

    Add files via upload

---
+++ Page/app.js
@@ -0,0 +1,62 @@
+  window.sr = ScrollReveal();
+    sr.reveal('.tekstbox2', {
+        reset: true,
+        delay: 200,
+        distance: 0
+    });
+    
+    
+    window.sr = ScrollReveal();
+    sr.reveal('p', {
+        reset: true,
+        delay: 200,
+        distance: 0
+    });
+    window.sr = ScrollReveal();
+    sr.reveal('.lista', {
+        reset: true,
+        delay: 200,
+        distance: 0
+    });
+    window.sr = ScrollReveal();
+    sr.reveal('#section2', {
+        reset: true,
+        delay: 500,
+        distance: 0
+    });
+    
+    
+    window.sr = ScrollReveal();
+    sr.reveal('h2', {
+        reset: true,
+        delay: 200,
+        distance: 0
+    });
+    window.sr = ScrollReveal();
+    sr.reveal('k', {
+        reset: true,
+        delay: 300,
+        distance: 0
+    });
+    
+    window.sr = ScrollReveal();
+    sr.reveal('#strzalka', {
+        reset: true,
+        delay: 500,
+        distance: 0
+    });
+    
+    
+    $(function() {
+        jQuery.scrollSpeed (100,800) ;
+        
+
+    });
+    
+    $(function() { 
+    
+       
+          jQuery.scrollSpeed(100, 800,'easeInOutBack');
+        
+        });
+        
---
+++ Page/appp.js
@@ -0,0 +1,28 @@
+const slider= document.querySelector('.slider');
+const slider2= document.querySelector('.slider2');
+const logo= document.querySelector('.logo');
+const shop= document.querySelector('.shop');
+const slajder=document.querySelector('.slajder');
+const nawigacja= document.querySelector('.nawigacja');
+const nawigacjaa= document.querySelector('.nawigacjaa');
+const nawigacjaaa= document.querySelector('.nawigacjaaa');
+const slajder2= document.querySelector('.slajder2');
+const slajder3= document.querySelector('header');
+const zdjecie= document.querySelector('.zdjecie');
+const zdjecie2= document.querySelector('.zdjecie1');
+
+const t1= new TimelineMax();
+
+t1.fromTo(slider,1.2,{x:"-100%"}, {x:"0%",ease:Power2.easeInOut},"-=1.2")
+.fromTo(slider2,1.2,{x:"100%"}, {x:"0%",ease:Power2.easeInOut},"-=1.2")
+.fromTo(slajder3,1.2,{x:"100%"}, {x:"0%",ease:Power2.easeInOut},"-=1.2")
+
+.fromTo(slajder,2.3,{x:"-200%"},{x:"0%",ease:Power2.easeInOut},"-=1.2")
+.fromTo(zdjecie,1.2,{opacity:0,y:30},{opacity:1,y:0,},"-=1.2")
+.fromTo(zdjecie2,1.2,{opacity:0,y:30},{opacity:1,y:0,},"-=1.2")
+.fromTo(slajder2,1.5,{opacity:0,x:10},{opacity:1,x:0},"-=0.2")
+.fromTo(logo,0.5,{opacity:0,x:10},{opacity:1,x:0},"-=0.9")
+.fromTo(shop,0.5,{opacity:0,x:30},{opacity:1,x:0},"-=0.9")
+.fromTo(nawigacja,1.2,{opacity:0,x:30},{opacity:1,x:0},"-=0.9")
+.fromTo(nawigacjaa,1.2,{opacity:0,x:30},{opacity:1,x:0},"-=1")
+.fromTo(nawigacjaaa,1.2,{opacity:0,x:30},{opacity:1,x:0},"-=1");
