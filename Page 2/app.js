73bc8f639ab14c34d704572b834d4f126c4dfc68
MaTTeK6 <62448676+MaTTeK6@users.noreply.github.com>
2020-05-02 14:50:45 +0200

    Delete app.js

--- Page 2/app.js
+++
@@ -1,9 +0,0 @@
-$(window).scroll(function(e) {
-    var currentposition = $(document).scrollTop(0);//aktualna pozycja względem początku          
-    var bannerheight = $(".nawigacja").height(0.7);//wysokość elementu, po którym menu ma stać się białe
-     if(currentposition>bannerheight)//odwrotny warunek
-      {        
-
-          $(".nawigacja").addClass("lover");
-      }
- });
